66a91faaecd57d7b3fc99e924305c7f6674a7e38
moazahmed22 <nabelnadel21@gmail.com>
2025-06-19 18:29:01 +0300

    first working version of the signup & login page with validations

---
+++ js/index.js
@@ -0,0 +1,237 @@
+const loginForm = document.querySelector("#loginForm");
+const signUpForm = document.querySelector("#signUpForm");
+const loginAlert = document.querySelector("#loginAlert");
+const signUpAlert = document.querySelector("#signUpAlert");
+const switchFormBtns = Array.from(document.querySelectorAll(".switchFormBtns"));
+const loginBtn = document.querySelector("#loginBtn");
+const signUpBtn = document.querySelector("#signUpBtn");
+const whiteOverlay = document.querySelector(".whiteOverlay");
+const loginFormInputs = Array.from(loginForm.querySelectorAll("input"));
+const signUpFormInputs = Array.from(signUpForm.querySelectorAll("input"));
+const overlayAlert = whiteOverlay.querySelector("p");
+let alertMessages = {
+  name: "name must be at least 2 characters",
+  email: "email is not valid",
+  password: "password is not valid",
+  login: "email or password is not correct",
+};
+
+// handel input validation in both forms
+signUpFormInputs.forEach((element) => {
+  element.addEventListener("input", (event) => {
+    signUpValidation(event.target);
+  });
+});
+
+loginFormInputs.forEach((element) => {
+  element.addEventListener("input", (event) => {
+    signUpValidation(event.target);
+  });
+});
+
+let usersDataBase = JSON.parse(localStorage.getItem("userDataBase")) || [];
+
+// handle signup button
+signUpBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  // validate inputs before sign up
+  if (
+    signUpFormInputs.every((element) => element.value !== "") &&
+    signUpFormInputs.every((element) =>
+      element.classList.contains("success-input")
+    )
+  ) {
+    addNewUser();
+    overlayAlert.textContent = "success";
+    signUpAlert.textContent = "success";
+  } else {
+    // handeling error messages on signup
+    let errorFields = [...signUpForm.querySelectorAll(".wrong-input")];
+    let errorMsgs = "";
+    errorFields.forEach((element) => {
+      switch (element.id) {
+        case "signUpName":
+          errorMsgs += `${alertMessages.name}, `;
+          break;
+        case "signUpEmail":
+          errorMsgs += `${alertMessages.email}, `;
+          break;
+        case "signUpPassword":
+          errorMsgs += `${alertMessages.password}, `;
+          break;
+      }
+    });
+    overlayAlert.textContent =
+      errorMsgs || "please signup using your name, account and password";
+    signUpAlert.textContent =
+      errorFields.length > 0 && "one or more inputs are invalid";
+  }
+});
+
+// function to add new users to the usersDataBase
+let addNewUser = () => {
+  if (
+    signUpForm.children[1].children[1].value != "" &&
+    signUpForm.children[2].children[1].value != "" &&
+    signUpForm.children[3].children[1].value != ""
+  ) {
+    let newUser = {
+      name: signUpForm.children[1].children[1].value,
+      email: signUpForm.children[2].children[1].value,
+      password: signUpForm.children[3].children[1].value,
+    };
+    usersDataBase.push(newUser);
+    cleanInputFields();
+    addUserToLocalStorage();
+  }
+};
+
+// function to add usersDataBase to local storage
+let addUserToLocalStorage = () => {
+  localStorage.setItem("usersDataBase", JSON.stringify(usersDataBase));
+};
+
+// function to clear input fields
+let cleanInputFields = () => {
+  let fields = Array.from(document.querySelectorAll("input"));
+  fields.forEach((element) => {
+    element.value = null;
+  });
+};
+
+// overlay slide animation
+switchFormBtns.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    whiteOverlay.classList.toggle("whiteOverlayAnimtation");
+    loginForm.classList.toggle("d-none");
+    loginForm.classList.toggle("d-flex");
+    signUpForm.classList.toggle("d-none");
+    signUpForm.classList.toggle("d-flex");
+    overlayAlert.textContent = signUpForm.classList.contains("d-none")
+      ? "please login using your email and password"
+      : `please signup using your name, account and password`;
+  });
+});
+
+// sign up form validation
+// validation styling on input
+let signUpValidation = (target) => {
+  let regex;
+  switch (target.id) {
+    case "signUpName":
+      regex = /^[A-Za-z\s]{2,}$/m;
+      if (regex.test(target.value)) {
+        target.classList.remove("wrong-input");
+        target.classList.add("success-input");
+        // signup alert on mobile
+        signUpAlert.classList.remove("text-alert");
+        signUpAlert.textContent = null;
+        // signup alert on overlay
+        overlayAlert.classList.remove("text-alert");
+        overlayAlert.textContent =
+          "please signup using your name, account and password";
+      } else {
+        target.classList.remove("success-input");
+        target.classList.add("wrong-input");
+
+        signUpAlert.classList.add("text-alert");
+        signUpAlert.textContent = alertMessages.name;
+
+        overlayAlert.classList.add("text-alert");
+        overlayAlert.textContent = alertMessages.name;
+      }
+      break;
+    case "signUpEmail":
+      regex = /^[A-Za-z]{2,}\@[A-Za-z]{2,}(\.[A-Za-z]{2,})?$/m;
+      if (regex.test(target.value)) {
+        target.classList.remove("wrong-input");
+        target.classList.add("success-input");
+        // alert on mobile login and signup
+        signUpAlert.classList.remove("text-alert");
+        signUpAlert.textContent = null;
+        // alert on overlay
+        overlayAlert.classList.remove("text-alert");
+        overlayAlert.textContent =
+          "please signup using your name, account and password";
+      } else {
+        target.classList.remove("success-input");
+        target.classList.add("wrong-input");
+        // alert on mobile signup
+        signUpAlert.classList.add("text-alert");
+        signUpAlert.textContent = alertMessages.email;
+        // alert on overlay
+        overlayAlert.classList.add("text-alert");
+        overlayAlert.textContent = alertMessages.email;
+      }
+      break;
+    case "loginEmail":
+      regex = /^[A-Za-z]{2,}\@[A-Za-z]{2,}(\.[A-Za-z]{2,})?$/m;
+      if (regex.test(target.value)) {
+        target.classList.remove("wrong-input");
+        target.classList.add("success-input");
+        // alert on mobile login
+        loginAlert.classList.remove("text-alert");
+        loginAlert.textContent = null;
+        // alert on overlay
+        overlayAlert.classList.remove("text-alert");
+        overlayAlert.textContent =
+          "please signup using your name, account and password";
+      } else {
+        target.classList.remove("success-input");
+        target.classList.add("wrong-input");
+        // alert on mobile login
+        loginAlert.classList.add("text-alert");
+        loginAlert.textContent = alertMessages.email;
+        // alert on overlay
+        overlayAlert.classList.add("text-alert");
+        overlayAlert.textContent = alertMessages.email;
+      }
+      break;
+    case "signUpPassword":
+      regex = /^\S+$/m;
+      if (regex.test(target.value)) {
+        target.classList.remove("wrong-input");
+        target.classList.add("success-input");
+        // alert on mobile login and signup
+        signUpAlert.classList.remove("text-alert");
+        signUpAlert.textContent = null;
+        // alert on overlay
+        overlayAlert.classList.remove("text-alert");
+        overlayAlert.textContent =
+          "please signup using your name, account and password";
+      } else {
+        target.classList.remove("success-input");
+        target.classList.add("wrong-input");
+        // alert on mobile login and signup
+        signUpAlert.classList.add("text-alert");
+        signUpAlert.textContent = alertMessages.password;
+        // alert on overlay
+        overlayAlert.classList.add("text-alert");
+        overlayAlert.textContent = alertMessages.password;
+      }
+      break;
+    case "loginPassword":
+      regex = /^\S+$/m;
+      if (regex.test(target.value)) {
+        target.classList.remove("wrong-input");
+        target.classList.add("success-input");
+        // alert on mobile login and signup
+        loginAlert.classList.remove("text-alert");
+        loginAlert.textContent = null;
+        // alert on overlay
+        overlayAlert.classList.remove("text-alert");
+        overlayAlert.textContent =
+          "please signup using your name, account and password";
+      } else {
+        target.classList.remove("success-input");
+        target.classList.add("wrong-input");
+        // alert on mobile login and signup
+        loginAlert.classList.add("text-alert");
+        loginAlert.textContent = alertMessages.password;
+        // alert on overlay
+        overlayAlert.classList.add("text-alert");
+        overlayAlert.textContent = alertMessages.password;
+      }
+      break;
+  }
+};
